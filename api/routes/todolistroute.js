f7a71210d2dca1482a30228d3bedc8a518223fd7
Ritdak Dimka <38168221+Ritdak@users.noreply.github.com>
2018-12-30 13:05:39 +0100

    todolist back-end

---
+++ api/routes/todolistroute.js
@@ -0,0 +1,15 @@
+'use strict'
+module.exports = function(app){
+	var todolist = require('../controllers/todolistcontroller')
+
+	// todolist routes
+
+	app.route('/tasks')
+	.get(todolist.list_all_tasks)
+	.post(todolist.create_a_task)
+
+	app.route('/tasks/:taskId')
+	.get(todolist.read_a_task)
+	.put(todolist.update_a_task)
+	.delete(todolist.delete_a_task);
+};
